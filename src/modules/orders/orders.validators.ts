3c0ae36d4cad666e0d0868b891fb50ca8f56766d
Alex Romero <alex1812r@gmail.com>
2022-06-12 20:50:10 -0400

    validate order date

--- src/modules/orders/orders.validators.ts
+++ src/modules/orders/orders.validators.ts
@@ -2,6 +2,7 @@ import * as yup from 'yup';
 
 export const orderSchema = yup.object({
   clientId: yup.number().min(1, 'client invalid').required(),
+  orderDate: yup.date().required(),
   orderProducts: yup.array(yup.object({
     productId: yup.number().min(1, 'product invalid').required(),
     count: yup.number().min(1, 'count required').required()
